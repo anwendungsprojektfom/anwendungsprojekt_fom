23585f002a3b1c320934a373c109c50f544a046e
Jonas Waigel <jonas.waigel@outlook.com>
2024-03-16 16:11:25 +0100

    Create homescreen with bottom bar menu

---
+++ sth_app/lib/pages/homescreen/homescreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text('This is our homescreen'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedIconTheme: const IconThemeData(color: Colors.blue),
+        unselectedIconTheme: const IconThemeData(color: Colors.grey),
+      ),
+    );
+  }
+}
