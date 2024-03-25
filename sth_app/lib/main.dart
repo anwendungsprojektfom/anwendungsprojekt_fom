e13b3a76bae728b30a3350ed4aee84f00d46d1b9
VasilisZour95 <121232621+VasilisZour95@users.noreply.github.com>
2024-03-25 13:05:08 +0100

    Loading Screen Done

--- sth_app/lib/main.dart
+++ sth_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sth_app/pages/chatscreen/chatscreen.dart';
 import 'package:sth_app/pages/homescreen/homescreen.dart';
+import 'package:sth_app/pages/loadingscreen/loadingscreen.dart';
 import 'package:sth_app/pages/profilescreen/profilescreen.dart';
 
 void main() {
@@ -11,10 +12,15 @@ class SthApp extends StatelessWidget {
   const SthApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'STH App', debugShowCheckedModeBanner: false, initialRoute: '/homescreen', routes: {
-      '/homescreen': (context) => const HomeScreen(),
-      '/chatscreen': (context) => const ChatScreen(),
-      '/profilescreen': (context) => const ProfileScreen(),
-    });
+    return MaterialApp(
+        title: 'STH App',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/loadingscreen',
+        routes: {
+          '/loadingscreen': (context) => const LoadingScreen(),
+          '/homescreen': (context) => const HomeScreen(),
+          '/chatscreen': (context) => const ChatScreen(),
+          '/profilescreen': (context) => const ProfileScreen(),
+        });
   }
 }
--- sth_app/lib/pages/loadingscreen/loadingscreen.dart
+++ sth_app/lib/pages/loadingscreen/loadingscreen.dart
@@ -1,1 +1,40 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:sth_app/pages/homescreen/homescreen.dart';
 
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreen createState() => _LoadingScreen();
+}
+
+class _LoadingScreen extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/homescreen');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text(
+              'Loading...',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
