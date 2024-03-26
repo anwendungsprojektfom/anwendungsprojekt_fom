8bc697a85e341322c1fce304dc7484d68c14c46d
DevHasan <hasan-deveci@web.de>
2024-03-27 00:13:17 +0100

    implemented getstream chat without widget working

--- sth_app/lib/main.dart
+++ sth_app/lib/main.dart
@@ -3,16 +3,54 @@ import 'package:sth_app/pages/chatscreen/chatscreen.dart';
 import 'package:sth_app/pages/homescreen/homescreen.dart';
 import 'package:sth_app/pages/profilescreen/profilescreen.dart';
 import 'package:sth_app/pages/searchscreen/searchscreen.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-void main() {
-  runApp(const SthApp());
+void main() async{
+  final client = StreamChatClient(
+    'ujgtsxqgs2kv',
+    logLevel: Level.INFO,
+  );
+  
+  await client.connectUser(User(id: 'test2'),
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidGVzdDIifQ.hfNj5j67fyf4z2UBwxQcOqQD6hT74F6bBY6x-aYXzrg');
+
+  final channel = client.channel('messaging', id: 'flutterdevs');
+
+  channel.watch();
+
+  runApp(SthApp(
+    client: client,
+    channel: channel,
+  ));
 }
 
 class SthApp extends StatelessWidget {
-  const SthApp({super.key});
+  const SthApp({
+    Key? key,
+    required this.client, 
+    required this.channel,
+    }) : super(key: key);
+
+    final StreamChatClient client;
+    final Channel channel;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'STH App', debugShowCheckedModeBanner: false, initialRoute: '/homescreen', routes: {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      builder: (context, widget) => StreamChat(
+          client: client, 
+          child: widget,
+          ),
+      home: StreamChannel(
+        channel: channel,
+        child: const ChatScreen()
+        ),
+      title: 'STH App', 
+      debugShowCheckedModeBanner: false, 
+      initialRoute: '/homescreen', 
+      routes: {
       '/homescreen': (context) => const HomeScreen(),
       '/chatscreen': (context) => const ChatScreen(),
       '/profilescreen': (context) => const ProfileScreen(),
--- sth_app/lib/pages/chatscreen/chatscreen.dart
+++ sth_app/lib/pages/chatscreen/chatscreen.dart
@@ -1,44 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:sth_app/technical/technical.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
-
+  
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _searchChatController = TextEditingController();
-  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'Chatscreen',
-        onBack: true,
-        showChatIcon: false,
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchChatController,
-              decoration: const InputDecoration(
-                hintText: 'Search...',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
-              ),
+    return const Scaffold(
+        appBar: StreamChannelHeader(),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamMessageListView(),
             ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('This is our chatscreen.'),
-            ),
-          ),
-        ],
-      ),
-    );
+            StreamMessageInput(),
+          ],
+        ));
   }
 }
