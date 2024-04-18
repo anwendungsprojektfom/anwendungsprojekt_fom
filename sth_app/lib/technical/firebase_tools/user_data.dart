ecc949db1ef16ea93a84b74595e4f84a05c2aa88
Jonas Waigel <jonas.waigel@outlook.com>
2024-04-18 20:14:44 +0200

    Add user_data class and add to technical

---
+++ sth_app/lib/technical/firebase_tools/user_data.dart
@@ -0,0 +1,11 @@
+class UserData {
+  final String name;
+  final String address;
+  final String email;
+
+  UserData({
+    required this.name,
+    required this.address,
+    required this.email,
+  });
+}
